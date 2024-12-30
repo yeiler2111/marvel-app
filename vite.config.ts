2b1058d0b069514ebaca53dca85181e98b0619fb
yeiler2111 <yeiler2209@gmail.com>
2024-12-30 18:34:35 -0500

    t

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/marvel-app/',  
   build: {
-    outDir: 'build', 
+    outDir: 'build',
   }
 })
