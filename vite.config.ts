556aa4503830dae9bf6aaa18f044a7a3cc03a0ae
yeiler2111 <yeiler2209@gmail.com>
2024-12-30 18:41:36 -0500

    cambiando a https peticiones

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/marvel-app/',  
   build: {
     outDir: 'build',
   }
