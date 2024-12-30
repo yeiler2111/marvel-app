10572a12e9e53ff6d8ffbee34ca0eb603a6be40c
yeiler2111 <yeiler2209@gmail.com>
2024-12-30 18:14:11 -0500

    seattings

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Register } from "../component/register/Register";
 import NotFoundPage from "../pages/NotFound/NotFoundPage";
 import DetailsCharacter from "../pages/characters/DetailsCharacter";
@@ -10,19 +10,17 @@ import DetailsSeries from "../pages/series/DetailsSerie";
 import SeriesLayout from "../pages/series/Series";
 function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/character/:id" element={<DetailsCharacter />} />
-        <Route path="/comics/:title" element={<DetailsSeries />} />
-        <Route path="/comics/:id" element={<DetailsComic />} />
-        <Route path="/series" element={<SeriesLayout />} />
-        <Route path="/comics" element={<Comics />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/character/:id" element={<DetailsCharacter />} />
+      <Route path="/comics/:title" element={<DetailsSeries />} />
+      <Route path="/comics/:id" element={<DetailsComic />} />
+      <Route path="/series" element={<SeriesLayout />} />
+      <Route path="/comics" element={<Comics />} />
+    </Routes>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
 import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.tsx";
 import "./index.module.css";
@@ -9,7 +10,9 @@ import { store } from "./store/store.ts";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
